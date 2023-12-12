0ba6b8431df3cbce863f65fac122888217f25e29
谢金虎 <1050780355@qq.com>
2023-12-12 10:44:10 +0800

    add

--- bigdata/start-services.sh
+++ bigdata/start-services.sh
@@ -3,21 +3,16 @@
 # 启动 MySQL
 service mysql start
 
-# 初始化 MySQL 数据库 (首次运行)
-# mysql < some-initial-script.sql
-
 # 启动 ZooKeeper
-$ZOOKEEPER_HOME/bin/zkServer.sh start
+/zookeeper/bin/zkServer.sh start
 
 # 启动 Hadoop
-$HADOOP_HOME/bin/hdfs namenode -format
-$HADOOP_HOME/sbin/start-dfs.sh
-$HADOOP_HOME/sbin/start-yarn.sh
+/hadoop/bin/start-dfs.sh
+/hadoop/bin/start-yarn.sh
 
 # 启动 Hive
-$HIVE_HOME/bin/hive --service metastore &
-$HIVE_HOME/bin/hive --service hiveserver2 &
+/hive/bin/hive --service metastore &
+/hive/bin/hive --service hiveserver2 &
 
 # 保持容器运行
 tail -f /dev/null
-
